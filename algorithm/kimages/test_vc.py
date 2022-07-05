fa577ae9815dc9fe4fb494f4f783e19bea0f774d
ewier <e.wierciak@gmail.com>
2022-07-05 22:53:30 +0200

    added run in gui

---
+++ algorithm/kimages/test_vc.py
@@ -0,0 +1,61 @@
+from vc import *
+from random import randint, shuffle
+
+def test_permute(n,m,vc: VC):
+    permutation = [i for i in range(m)]
+    shuffle(permutation)
+    S = [[randint(1,100) for j in range(m)] for i in range(n)]
+    newS = vc.permute(S, permutation)
+    print(f"S = {S}\np = {permutation}\n=> {newS}")
+
+def test_simple():
+    vc = VC(3,3)
+    array = np.asarray([   [BLACK, OTHER, OTHER],
+                [BLACK, BLACK, BLACK],
+                [OTHER, BLACK, OTHER] ])
+    img = CImage()
+    img.update_matrix(array)
+    img.width, img.height = 3,3
+    img.update_image()
+
+    shares = vc(img)
+    decryptedImg = vc.combineShares()
+    decryptedImg.show_image()
+
+def test_image(filename = "really-big-picture.jpg"):
+    vc = VC(3,3)
+    print("--init")
+    path = f".\\algorithm\\k-images\\test_img\\{filename}"
+    img = CImage()
+    img.read_image(path)
+    img.show_image()
+    print("--run")
+    shares = vc(img)
+    for share in shares:
+        share.show_image()
+    print("--shares")
+    decryptedImg = vc.combineShares()
+    print("--combined")
+    decryptedImg.show_image()
+
+def run(n = 3, path = ".\\algorithm\\k-images\\test_img\\penta.png"): #D:\\Rok_Akademicki_21-22\\Praca_Inzynierska\\Official_Repo\\vCryptography
+    vc = VC(n,n)
+    img = CImage()
+    img.read_image(path)
+    shares = vc(img)
+    decryptedImg = vc.combineShares()
+    return shares,decryptedImg
+
+    
+
+
+# test_simple()
+# test_image("penta.png")
+
+# vc = VC(3,3)
+# test_permute(3,3,vc)
+
+# shares,dec = run()
+# for s in shares:
+#     s.show_image()
+# dec.show_image()
--- gui/KImagesFrame.py
+++ gui/KImagesFrame.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QFrame, QSizePolicy, QVBoxLayout, QLabel
-
+from PyQt5.QtWidgets import QFrame, QSizePolicy, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QRect
+from algorithm.kimages.test_vc import run
 from gui import MainMenuWindow
 
 
@@ -10,16 +12,38 @@ class KImagesFrame(QFrame):
         self.initUI()
 
     def initUI(self):
+        self.path = "algorithm\\kimages\\test_img\\penta.png"
         self.setStyleSheet("background:#3a3a3a; border: 3px solid #323232;")
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         layout = QVBoxLayout()
-        title = QLabel("vCryptography - 2")
-        layout.addWidget(title)
+        # title = QLabel("vCryptography - 2")
+        # layout.addWidget(title)
+        self.photo = QLabel()
+        self.photo.setGeometry(QRect(0,0,421,256)) #0,0,421,256 #0, 0, 841, 511
+        self.photo.setPixmap(QPixmap(self.path))
+        self.photo.setScaledContents(True)
+        self.left = QPushButton()
+        self.left.setGeometry(QRect(0,255,206,21)) #0,255,206,21 #0, 510, 411, 41
+        self.right = QPushButton()
+        self.right.setGeometry(QRect(205,255,191,21)) #205,255,191,21 #410, 510, 391, 41
+        self.left.clicked.connect(self.show_picture)
+        self.right.clicked.connect(self.show_picture)
+        layout.addWidget(self.photo)
+        layout.addWidget(self.left)
+        layout.addWidget(self.right)
+
         self.setLayout(layout)
         self.setVisible(False)
+    
+    def show_picture(self):
+        self.photo.setPixmap(QPixmap("algorithm\\kimages\\test_img\\really-big-picture.jpg"))
 
     def show(self):
         self.setVisible(True)
 
     def hide(self):
         self.setVisible(False)
+
+    def run(sel,n,k,orig_path):
+        shares,decrypted_image = run(n,orig_path)
+        return shares,decrypted_image
